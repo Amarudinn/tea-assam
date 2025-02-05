10bc7ab8446530285416120e111bedc4a536c204
Amarudinn <masamar707@gmail.com>
2025-02-05 10:38:32 +0700

    update

--- sportsbetting/sports.js
+++ sportsbetting/sports.js
@@ -336,8 +336,8 @@ async function withdrawEth() {
     }
 }
 
-const startDate = new Date(2025, 1, 4, 13, 0, 0); // January 25, 2025, 20:18:00
-    const endDate = new Date(2025, 1, 5, 1, 0, 0);   // January 25, 2025, 20:30:00
+const startDate = new Date(2025, 1, 5, 11, 0, 0); // January 25, 2025, 20:18:00
+    const endDate = new Date(2025, 1, 6, 7, 0, 0);   // January 25, 2025, 20:30:00
     const timerDisplay = document.getElementById("timerDisplay");
     const input = document.getElementById("betAmount");
     const buttons = [
